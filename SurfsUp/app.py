597b50bd7a895360088032fef212cb2e4ca57805
MrGoots <134533117+MrGoots@users.noreply.github.com>
2023-09-06 19:27:53 -0700

    alchemy

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -1,19 +1,27 @@
 # Import the dependencies.
-
+import numpy as np
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+from flask import Flask, jsonify
+import datetime as dt
+from dateutil.relativedelta import relativedelta
 
 
 #################################################
 # Database Setup
 #################################################
-
+engine = create_engine('sqlite:///./Resources/hawaii.sqlite')
 
 # reflect an existing database into a new model
-
+Base = automap_base()
 # reflect the tables
-
+Base.prepare(engine, reflect=True)
 
 # Save references to each table
-
+Measurement = Base.classes.measurement
+Station = Base.classes.station
 
 # Create our session (link) from Python to the DB
 
@@ -21,10 +29,23 @@
 #################################################
 # Flask Setup
 #################################################
-
+setup = Flask(__name__,static_url_path='UCI_DABC_SQLAlchemy/SurfsUp/Surfs_up.jpeg')
 
 
 
 #################################################
 # Flask Routes
 #################################################
+@app.route("/")
+def home():
+    """All Available API Routes List:"""
+    return (
+        f"<h1>This is a Flask API for the Climate App</h1"
+        f"<h2>Available API Routes:</h2"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/start<br/>"
+        f"/api/v1.0/start/end<br/>"
+
+    )
